be7e43c72566632365d82d5151f38254d407ac97
rishikisgood <Kaulrishik15@gmail.com>
2025-05-22 15:23:13 +0530

    Add files via upload

---
+++ Swap.py
@@ -0,0 +1,17 @@
+
+
+a = 10
+b = 20
+c = 30
+
+print("Actual Value:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
+a, b, c = c, a, b
+
+print("\nSwapped values:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
